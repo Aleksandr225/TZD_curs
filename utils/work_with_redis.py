02b1aaf2ffacf16784c9b8773e0e83f1268eb3da
a666vg <abvgdedjzi@yandex.ru>
2025-12-07 23:14:14 +0300

    fix redis env

--- utils/work_with_redis.py
+++ utils/work_with_redis.py
@@ -1,3 +1,8 @@
+import os
 import redis
-r = redis.Redis(host='localhost', port=6379, db=0)
-r.ping()
+r = redis.Redis(
+    host=os.getenv('REDIS_HOST', 'localhost'),
+    port=int(os.getenv('REDIS_PORT', 6379)),
+    db=0,
+    decode_responses=True
+)
